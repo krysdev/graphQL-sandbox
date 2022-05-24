c8332b66eb1698229628c7e2dbad3b4186a29a0f
KrysDev <webdevkrys@gmail.com>
2022-05-24 22:03:27 +0100

    schema

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
 
-const app = express()
+const app = express();
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
 
 app.listen(3000, () => {
-    console.log(333);
+    console.log('xxxxx');
 })
